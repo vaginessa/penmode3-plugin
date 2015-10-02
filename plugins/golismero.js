366556fae9ec1500c3c1d33d5b794854cfb14dcb
thez3ro <silvethebest@yahoo.it>
2015-10-02 20:46:23 +0200

    golismero update

--- plugins/golismero.js
+++ plugins/golismero.js
@@ -3,28 +3,38 @@ var process = null;
 
 var execute = function (engine, cb) {
   engine.started();
-  process = spawn('python', ['~/Dev/golismero-master/golismero.py', 'scan', 'localhost', '-vvvv']);
-  engine.console('> python golismero.py scan localhost -vvvv');
-  
-  process.stdout.on('data', function (data) {
-    if (data) {
-      engine.console(data);
+  var input = '[{"param":"address"}]';
+  engine.request_io(input);
+  engine.once('io', function (obj) {
+    obj = JSON.parse(obj);
+    if (typeof obj.address != 'undefined' && obj.address != '') {
+      process = spawn('golismero', ['scan', 'localhost', '-vvvv']);
+      engine.console('> golismero scan localhost -vvvv');
+      engine.setInteractive(true);
+
+      process.stdout.on('data', function (data) {
+        if (data) {
+          engine.console(data);
+        }
+      });
+
+      process.stderr.on('data', function (data) {
+        if (data) {
+          engine.console(data);
+        }
+      });
+
+      process.on('exit', function () {
+        return cb(engine.ended());
+      });
+
+      engine.on('command', function (msg) {
+        process.stdin.write(msg + '\r');
+      });
+    } else {
+      engine.fail('\'address\' parameter can\'t be empty');
+      return cb(engine.ended());
     }
-  });
-
-  process.stderr.on('data', function (data) {
-    if (data) {
-      engine.console(data);
-    }
-  });
-
-  process.on('exit', function () {
-    process = null;
-    return cb(engine.ended());
-  });
-
-  engine.on('command', function (msg) {
-    process.stdin.write(msg + '\r');
   });
 };
 
