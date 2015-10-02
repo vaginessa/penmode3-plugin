63c07bfd8d6da7ed211fd4371ac6eed402254e68
b4dtR1p <pucci.alessandro84@gmail.com>
2015-10-02 19:03:39 +0200

    try to fix golismero output

--- plugins/golismero.js
+++ plugins/golismero.js
@@ -1,33 +1,29 @@
-var spawn = require('child_process').spawn;
-var process = null;
+var exec = require('child_process').exec;
 
 var execute = function (engine, cb) {
   engine.started();
-  process = spawn('python', ['~/Dev/golismero-master/golismero.py', 'scan', 'localhost', '-vvvv']);
-  engine.console('> python golismero.py scan localhost -vvvv');
-  
-  process.stdout.on('data', function (data) {
-    if (data) {
-      engine.console(data);
-    }
-  });
-
-  process.stderr.on('data', function (data) {
-    if (data) {
-      engine.console(data);
-    }
-  });
-
-  process.on('exit', function () {
-    process = null;
-    return cb(engine.ended());
-  });
-
-  engine.on('command', function (msg) {
-    process.stdin.write(msg + '\r');
+  var input = '[{"param":"address"}]';
+  engine.request_io(input);
+  engine.once('io', function (obj) {
+    obj = JSON.parse(obj);
+    exec('python ~/Dev/golismero-master/golismero.py scan ' + obj.address + ' -vvvv', function (error, stdout, stderr) {
+      if (error) {
+        throw error;
+      }
+      engine.console('> python golismero.py scan ' + obj.address + ' -vvvv');
+      
+      engine.on('command', function (msg) {
+        process.stdin.write(msg + '\r');
+      });
+     
+      engine.console(stdout);
+      engine.console(stderr);
+      return cb(engine.ended());
+    });
   });
 };
 
 module.exports = {
   'execute': execute
 };
+
