b6f28df7b367742a3b977a9bda387163363c3dcc
thez3ro <silvethebest@yahoo.it>
2015-10-07 19:31:05 +0200

    whois + nmap fix

--- plugins/whois.js
+++ plugins/whois.js
@@ -2,13 +2,13 @@ var exec = require('child_process').exec;
 
 var execute = function (engine, cb) {
   engine.started();
-  var input = '[{"param":"parameters"}]';
+  var input = '[{"param":"address"}]';
   engine.request_io(input);
   engine.once('io', function (obj) {
     obj = JSON.parse(obj);
-    if (typeof obj.parameters != 'undefined' && obj.parameters != '') {
-      engine.console('> nmap ' + obj.parameters);
-      exec('nmap ' + obj.parameters, function (error, stdout, stderr) {
+    if (typeof obj.address != 'undefined' && obj.address != '') {
+      engine.console('> whois -H ' + obj.address);
+      exec('whois ' + obj.address, function (error, stdout, stderr) {
         if (error) {
           engine.fail(error.message);
         }
